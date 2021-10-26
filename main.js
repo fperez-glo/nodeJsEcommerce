7ebb7a2dc16f6685fa09f044491d82961590b1eb
fperez-glo <facundoperez6@gmail.com>
2021-10-25 21:11:27 -0300

    Se agrega la configuracion de multer para poder realizar carga de archivos.

--- main.js
+++ main.js
@@ -1,8 +1,29 @@
 const express = require(`express`);
-
+const multer = require('multer')
 
 const app = express();
 const port = process.env.PORT || 8080;
+
+//Configurar multer para poder recibir archivos con distintos formatos.
+const storage = multer.diskStorage({
+
+    destination: (req, file, cb)=> {
+        //recibe el nombre de la carpeta donde se va a guardar la informacion.
+        cb(null, './multer/update');
+    },
+    filename: (req, file, cb)=> {
+        //recibe el nombre con el cual se va a guardar el archivo.
+        cb(null, `file_${file.originalname}`);
+    },
+});
+
+const update = multer({ storage }); // Middleware
+
+app.post('/', update.single('fileUpload'), (req, res) => {
+    console.log(req.file)
+    res.send(`Archivo guardado con exito.`)
+});
+
 const prodRoutes = require('./router/routes/products/products');
 const cartRoutes = require('./router/routes/cart/cart');
 const authRoutes = require('./router/routes/auth/auth');
