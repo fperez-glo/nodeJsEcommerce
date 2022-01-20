b3a4d98e8a7b31e40e391af364ebed1964edb0ea
fperez-glo <facundoperez6@gmail.com>
2022-01-19 21:21:38 -0300

    compression en el servidor productivo

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
+require('dotenv').config();
 const express = require('express');
+const compression = require('compression');
+
+const { NODE_ENVIORMENT } = process.env;
 
 const app = express();
 
+//Permite la compresion del servidor.
+//Esto se utiliza en produccion para optimizar la velocidad de la aplicacion.
+app.use(compression());
+
+
+
 app.get('/', (req,res) => {
-    res.send('hola desde /')
+    if (NODE_ENVIORMENT === "prod") {
+        return res.send('hola desde prod');
+    }
+
+    res.send('hola 1 sola vez')
+    
 })
 
 app.listen(8080, () => {
