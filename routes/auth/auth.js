c7b5bea6718b073e7ad9180d5441012d701df957
fperez-glo <facundoperez6@gmail.com>
2022-01-21 22:47:54 -0300

    Se corrige el problema con passport

--- routes/auth/auth.js
+++ routes/auth/auth.js
@@ -1,54 +1,10 @@
 const express = require(`express`);
 const { Router } = express;
 const router = new Router();
+const { userDao } = require('../../daos/index');
 const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
+require('../../services/passport-local')
 
-const { userDao } = require('../../daos/index')
-
-//PASSPORT LOCAL
-passport.use('local-login', new LocalStrategy( async (user, password, done) => {
-    console.log('llega al local-login')
-    const registeredUser = await userDao.findUser({user, password});
-    
-    if (registeredUser.length) {
-        return done(null, registeredUser)
-    };
-    
-    done(null, false);
-    
-}))
-
-passport.use('local-signup', new LocalStrategy({
-    usernameField: 'user',
-    passwordField: 'password',
-    passReqToCallback: true,
-}, async (req ,user, password, done) => {
-    const registeredUser = await userDao.findUser({user, password});
-    if (registeredUser.length) {
-        console.log('El usuario ya existe')
-        return done(null, false)
-    }
-
-    await userDao.save({user, password});
-    
-    const createdUser = await userDao.findUser({user, password});
-    console.log('LLEGA A CREAR EL USUARIO:', createdUser)
-    done(null, createdUser);
-    
-}))
-
-passport.serializeUser((user, done) =>{
-    console.log('user ID:', user[0]._id)
-    done(null, user[0]._id)
-})
-
-passport.deserializeUser(async (id, done) => {
-    console.log('entra a deserializar el usuario con id:', id)
-    const deserializedUser = await userDao.findUser({id});
-    
-    done(null, deserializedUser);
-})
 
 router.get('/', (req, res) => {
     if (req.session.authorized){
@@ -74,29 +30,30 @@ router.post('/authSignUp', passport.authenticate('local-signup', {
 //RUTA para probar el login con passport
 router.post('/passportLogin', passport.authenticate('local-login', {
     successRedirect: '/home',
-    failureRedirect: '/authSignUp'
+    failureRedirect: '/authSignUp',
 }))
 
-router.post('/authLogIn', async (req,res)=> {
-    if(req.session.authorized){
-        res.redirect('/home');
-    } else {
+// router.post('/authLogIn', async (req,res)=> {
+//     if(req.session.authorized){
+//         res.redirect('/home');
+//     } else {
         
-        const registeredUser = await userDao.findUser(req.body);
-        console.log('registeredUser:', {id: registeredUser[0]._id, user: registeredUser[0].user})
-        if (registeredUser.length) {
-            req.session.user = registeredUser[0].fieldName;
-            req.session.authorized = true;
-            res.redirect('/home');
-        } else {
-            res.send('Usuario o contraseña incorrecto.')
-        };
-    };
+//         const registeredUser = await userDao.findUser(req.body);
+//         console.log('registeredUser:', {id: registeredUser[0]._id, user: registeredUser[0].user})
+//         if (registeredUser.length) {
+//             req.session.user = registeredUser[0].fieldName;
+//             req.session.authorized = true;
+//             res.redirect('/home');
+//         } else {
+//             res.send('Usuario o contraseña incorrecto.')
+//         };
+//     };
     
-})
+// })
 
 router.post('/authLogOut', (req,res)=> {
     if(req.session.authorized){
+        console.log('ENTRA AL LOGOUT')
         // req.session.authorized = false;
         // delete req.session.user;
         // delete req.session.authorized;
@@ -106,6 +63,7 @@ router.post('/authLogOut', (req,res)=> {
             }
             res.redirect('/')
         })
+        
     } else {
         res.redirect('/');
     };
---
+++ services/passport-local.js
@@ -0,0 +1,51 @@
+const LocalStrategy = require('passport-local').Strategy;
+const passport = require('passport');
+const { userDao } = require('../daos/index');
+
+//PASSPORT LOCAL
+passport.use('local-login', new LocalStrategy({
+    usernameField: 'user',
+    passwordField: 'password',
+    passReqToCallback: true
+} ,async (req,user, password, done) => {
+
+    const registeredUser = await userDao.findUser({user, password});
+    
+    if (registeredUser.length) {
+        req.session.authorized = true;
+        req.session.user = registeredUser[0].fieldName;
+        return done(null, registeredUser);
+    };
+
+    done(null, false);
+}))
+
+passport.use('local-signup', new LocalStrategy({
+    usernameField: 'user',
+    passwordField: 'password',
+    passReqToCallback: true,
+}, async (req ,user, password, done) => {
+    const registeredUser = await userDao.findUser({user, password});
+    if (registeredUser.length) {
+        console.log('El usuario ya existe')
+        return done(null, false)
+    }
+
+    await userDao.save({user, password});
+    
+    const createdUser = await userDao.findUser({user, password});
+    done(null, createdUser);
+    
+}));
+
+passport.serializeUser((user, done) =>{
+    done(null, user[0]._id)
+});
+
+passport.deserializeUser(async (id, done) => {
+    const deserializedUser = await userDao.findUser({id});
+    
+    done(null, deserializedUser);
+});
+
+module.exports = passport;
