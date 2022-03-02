6bfa779d0620459d41510c3730154441bb8cd9a2
fperez-glo <facundoperez6@gmail.com>
2022-03-02 12:14:33 -0300

    prueba para heroku4

--- src/helpers/crypto.js
+++ src/helpers/crypto.js
@@ -1,30 +1,31 @@
 //const { createDecipheriv, createCipheriv, scryptSync } = await import('crypto');
-import { createDecipheriv, createCipheriv, scryptSync } from 'crypto';
-import 'dotenv/config';
-const { CRYPTO_SECRET } = process.env;
 
-const algorithm = "aes-256-cbc";
-const iVector = scryptSync(CRYPTO_SECRET, 'GfG', 16);
-const Securitykey = scryptSync(CRYPTO_SECRET, 'GfG', 32);   
+// import { createDecipheriv, createCipheriv, scryptSync } from 'crypto';
+// import 'dotenv/config';
+// const { CRYPTO_SECRET } = process.env;
 
-export const encrypt = (inputToEncode) => {
+// const algorithm = "aes-256-cbc";
+// const iVector = scryptSync(CRYPTO_SECRET, 'GfG', 16);
+// const Securitykey = scryptSync(CRYPTO_SECRET, 'GfG', 32);   
 
-    //Encriptador
-    const cipher = createCipheriv(algorithm, Securitykey, iVector);
+// export const encrypt = (inputToEncode) => {
 
-    let dataEncrypted = cipher.update(inputToEncode, 'utf-8', 'hex');
-    return dataEncrypted += cipher.final("hex");
-};
+//     //Encriptador
+//     const cipher = createCipheriv(algorithm, Securitykey, iVector);
 
-export const  decrypt = (inputToDecode) => {
-    //Desencriptador
-    const decipher = createDecipheriv(algorithm, Securitykey, iVector);
+//     let dataEncrypted = cipher.update(inputToEncode, 'utf-8', 'hex');
+//     return dataEncrypted += cipher.final("hex");
+// };
 
-    let dataDecrypted = decipher.update(inputToDecode, 'hex', 'utf-8');
-    return dataDecrypted += decipher.final('utf8');
-};
+// export const  decrypt = (inputToDecode) => {
+//     //Desencriptador
+//     const decipher = createDecipheriv(algorithm, Securitykey, iVector);
 
-export const compare = (notEncryptedInput, encryptedInput) => {
-    const decryptInput = decrypt(encryptedInput);
-    return decryptInput === notEncryptedInput  ?  true : false;
-};
+//     let dataDecrypted = decipher.update(inputToDecode, 'hex', 'utf-8');
+//     return dataDecrypted += decipher.final('utf8');
+// };
+
+// export const compare = (notEncryptedInput, encryptedInput) => {
+//     const decryptInput = decrypt(encryptedInput);
+//     return decryptInput === notEncryptedInput  ?  true : false;
+// };
